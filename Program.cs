d900d5812ffcdeb5f8bd54a72b4a8bbdd6de5e29
Matyas Zsombor <mzsombi12345@gmail.com>
2023-12-30 11:25:29 +0100

    Created a simple map generator.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        File.WriteAllLines("/home/poci/Desktop/C++/renderer/map.txt", GenerateMap(24, 24));
+    }
+
+    static string[] GenerateMap(int width, int height)
+    {
+        string[] map = new string[height];
+        for (int i = 0; i < height; i++)
+        {
+            string line = "";
+            for (int j = 0; j < width; j++)
+            {
+                if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                {
+                    line += "1,255,0,0;";
+                }
+                else
+                {
+                    line += "0;";
+                }
+            }
+
+            map[i] = line;
+        }
+
+        return map;
     }
 }
